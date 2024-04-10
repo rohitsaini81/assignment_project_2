6fdf77cb07b446fea4657c61b16fcc8f675fc828
rohit <rohit@kali.kali>
2024-04-10 15:41:42 +0530

    all fixes spposed to be done

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'Home.html'));
 });
 app.get('/cart', (req, res) => {
     res.send(stocks.cart_items)
